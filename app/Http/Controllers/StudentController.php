cf72fd80cf96781039f68ab20341b24d4dd1ba07
Vikas Katariya <vikas.katariya@mindinventory.com>
2025-01-23 11:37:47 +0530

    changes for initial commit

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\SectionResource;
+use App\Models\Classes;
+use App\Models\Section;
+use App\Models\Student;
+use Illuminate\Http\Request;
+use App\Http\Resources\ClassResource;
+use App\Http\Resources\StudentResource;
+use Illuminate\Database\Eloquent\Builder;
+use App\Http\Requests\StoreStudentRequest;
+use App\Http\Requests\UpdateStudentRequest;
+
+class StudentController extends Controller
+{
+    public function index(Request $request)
+    {
+        $studentQuery = Student::search($request);
+        $classes = ClassResource::collection(Classes::all());
+        $sections = SectionResource::collection(Section::all());
+        $message = session('success');
+        return inertia('Student/Index', [
+            'students' => StudentResource::collection(
+                $studentQuery->paginate(5)
+            ),
+            'classes' => $classes,
+            'sections' => $sections,
+            'message' => $message,
+            'search' => request('search') ?? ''
+        ]);
+    }
+
+    protected function applySearch(Builder $query, $search)
+    {
+        return $query->when($search, function ($query, $search) {
+            $query->where('name', 'like', '%' . $search . '%');
+        });
+    }
+
+    public function create()
+    {
+        $classes = ClassResource::collection(Classes::all());
+
+        return inertia('Student/Create', [
+            'classes' => $classes
+        ]);
+    }
+
+    public function store(StoreStudentRequest $request)
+    {
+        Student::create($request->validated());
+        session()->flash('success', 'Student created successfully!');
+        return redirect()->route('students.index');
+    }
+
+    public function edit(Student $student)
+    {
+        $classes = ClassResource::collection(Classes::all());
+
+        return inertia('Student/Edit', [
+            'student' => StudentResource::make($student),
+            'classes' => $classes
+        ]);
+    }
+
+    public function update(UpdateStudentRequest $request, Student $student)
+    {
+        $student->update($request->validated());
+        session()->flash('success', 'Student updated successfully!');
+        return redirect()->route('students.index');
+    }
+
+    public function destroy(Student $student)
+    {
+        $student->delete();
+        session()->flash('success', 'Student deleted successfully!');
+        return redirect()->route('students.index');
+    }
+}
